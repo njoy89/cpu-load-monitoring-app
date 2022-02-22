e3e0e27862e99ed764e256c2ff9fea92b20d114b
Grzegorz Swatowski <grzegorz@sumologic.com>
2022-02-23 00:43:00 +0100

    .gitignore

--- src/backendCommunication/initCypress.ts
+++ src/backendCommunication/initCypress.ts
@@ -7,6 +7,13 @@ import { Action } from '../state/actions';
 export const initCypress = (
   dispatch: ThunkDispatch<State, undefined, Action>
 ) => {
+  dispatch({
+    type: 'ChangeConnectionState',
+    state: {
+      type: 'open',
+    },
+  });
+
   (window as any).Cypress.addDataPoint = (action: IncomingAction) => {
     dispatch({
       type: 'AddDataPoint',
